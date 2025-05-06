8d51ae2e59c8739b927018c541f8c78c87290fed
Adam Case <ajcase@gmail.com>
2025-05-06 14:32:52 -0500

    Fix

--- server/src/index.js
+++ server/src/index.js
@@ -83,6 +83,9 @@ app.use(helmet());
 app.use(cors());
 app.use(express.json());
 
+// Trust proxy for rate limiting behind reverse proxy
+app.set('trust proxy', 1);
+
 // Rate limiting
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
